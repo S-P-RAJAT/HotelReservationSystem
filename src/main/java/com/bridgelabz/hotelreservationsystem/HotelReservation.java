7734ef11a37d06ddaf1c1b6bde56c9042a8d9004
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-16 22:31:58 +0530

    ADDED: Functionality to find the cheapest hotel for given date range

--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservation.java
@@ -1,17 +1,33 @@
 package com.bridgelabz.hotelreservationsystem;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class HotelReservation {
     Hotel hotel;
-    ArrayList<Hotel> hotels = new ArrayList<>();
+    ArrayList<Hotel> hotelList = new ArrayList<>();
 
-    public void addHotels(String hotelName, int rating, CustomerType customerType, int rate) {
+
+    public void addHotel(String hotelName, int rating, CustomerType customerType, int rate) {
 
         hotel = new Hotel(hotelName, rating, customerType, rate);
-        hotels.add(hotel);
+        hotelList.add(hotel);
     }
     public int getSize(){
-        return hotels.size();
+        return hotelList.size();
+    }
+
+    public Hotel getCheapestHotel(LocalDateTime startDate, LocalDateTime endDate) {
+        int noOfDaysBetween = (int) ChronoUnit.DAYS.between(startDate, endDate);
+        Hotel hotel = hotelList.stream()
+                .min(Comparator.comparing(Hotel::getRate))
+                .orElse(null);
+
+        int cheapestPrice=hotel.getRate();
+        System.out.println("Hotel name: "+hotel.getHotelName()+" \nCheapest price: "+cheapestPrice*noOfDaysBetween);
+        return hotel;
+
     }
 }

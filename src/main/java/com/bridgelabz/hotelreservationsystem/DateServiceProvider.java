dd1f286cb888ce659fd599db63f91c38ed795948
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-18 02:39:07 +0530

    ADDED: Custom Exception tells users about invalid date format & range

--- src/main/java/com/bridgelabz/hotelreservationsystem/DateServiceProvider.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/DateServiceProvider.java
@@ -2,22 +2,64 @@ package com.bridgelabz.hotelreservationsystem;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.regex.Pattern;
+
+import com.bridgelabz.hotelreservationsystem.InvalidDateException.*;
 
 public class DateServiceProvider {
 
-    public static int calculateTotalDays(LocalDate startDate, LocalDate endDate) {
-        return endDate.plusDays(1).compareTo(startDate);
+    private static final String DATE_PATTERN = "^([0-2][0-9]|(3)[0-1])(Jan|Feb|Mar|Apr|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\d{4}$";
+
+    public static LocalDate dateParser(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+        return LocalDate.parse(date, formatter);
+
     }
-    public static long calculateWeekDays(LocalDate startDate, LocalDate endDate) {
+    public static int calculateTotalDays(String startDate, String endDate) {
 
-        final DayOfWeek startDay = startDate.getDayOfWeek();
-        final DayOfWeek endDay = endDate.plusDays(1).getDayOfWeek();
+        return dateParser(endDate).plusDays(1).compareTo(dateParser(startDate));
+    }
 
-        final long days = ChronoUnit.DAYS.between(startDate, endDate);
-        final long daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
+    public static int calculateWeekDays(String startDate, String endDate) {
+
+        LocalDate startingDate = dateParser(startDate);
+        LocalDate endingDate = dateParser(endDate);
+        final DayOfWeek startDay = startingDate.getDayOfWeek();
+        final DayOfWeek endDay = endingDate.plusDays(1).getDayOfWeek();
+
+        final int days = (int) ChronoUnit.DAYS.between(startingDate, endingDate.plusDays(1));
+        final int daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
 
         //adjust for starting and ending on a Sunday:
         return daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0) + (endDay == DayOfWeek.SUNDAY ? 1 : 0);
+    }
+
+    public static boolean validateFormatOfDate(String date) {
+
+        Pattern pattern = Pattern.compile(DATE_PATTERN);
+        return pattern.matcher(date).matches();
+
+    }
+
+
+
+    public static void dateValidation(String startDate, String endDate) {
+        try {
+            if (startDate.isEmpty() || endDate.isEmpty()) {
+                throw new InvalidDateException(ExceptionType.ENTERED_EMPTY, "Please enter valid date");
+            } else if (!validateFormatOfDate(endDate) || !validateFormatOfDate(startDate)) {
+                throw new InvalidDateException(ExceptionType.INVALID_DATE_FORMAT, "Enter valid date order");
+
+            } else if (dateParser(endDate).compareTo(dateParser(startDate)) <= 0) {
+                throw new InvalidDateException(ExceptionType.INVALID_DATES_ORDER, "Enter valid date order");
+
+            }
+
+        } catch (NullPointerException e) {
+            throw new InvalidDateException(ExceptionType.ENTERED_NULL, "Please enter valid date format");
+        }
+
     }
 }
---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/InvalidDateException.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class InvalidDateException extends RuntimeException {
+    enum ExceptionType {
+        ENTERED_NULL, ENTERED_EMPTY, INVALID_DATE_FORMAT, INVALID_DATES_ORDER
+    }
+
+    ExceptionType type;
+
+    public InvalidDateException(ExceptionType type, String message) {
+
+        super(message);
+        this.type = type;
+
+    }
+
+
+}
